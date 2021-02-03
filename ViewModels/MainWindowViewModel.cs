4ab1fbc562fac31693bfc6179e5ae4949ffa50f1
Rukin2018D <Rukin2018D@yandex.ru>
2021-02-03 19:12:37 +0300

    Интерфейс для лабы. Используется MVVM

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KantorLr1.ViewModels.Base;
+
+namespace KantorLr1.ViewModels
+{
+	public class MainWindowViewModel : BaseViewModel
+	{
+		private string status = "Hello";
+		public string Status { get => status; set => Set(ref status, value); }
+
+		private string title = "GaussMethod";
+		public string Title { get => title; set => Set(ref title, value); }
+	}
+}
