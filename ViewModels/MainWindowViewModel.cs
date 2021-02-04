7bb34b2aaf40be8422d48c41dbcf9e0a7a87e524
Rukin Danil <rukin2018d@yandex.ru>
2021-02-04 20:31:30 +0700

    прикрутил либу, зкончил интерфейс, надо пилить viewModel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using KantorLr1.ViewModels.Base;
+using CompMathLibrary;
+using CompMathLibrary.Methods;
 
 namespace KantorLr1.ViewModels
 {
 	public class MainWindowViewModel : BaseViewModel
 	{
+		public MainWindowViewModel()
+		{
+			CMReshala reshala = new CMReshala();
+		}
 		private string status = "Hello";
 		public string Status { get => status; set => Set(ref status, value); }
 
 		private string title = "GaussMethod";
 		public string Title { get => title; set => Set(ref title, value); }
+
 	}
 }
