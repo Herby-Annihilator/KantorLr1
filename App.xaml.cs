2054deb31e684e0a116b34aba2b9afa1e0aee14c
Rukin Danil <rukin2018d@yandex.ru>
2021-03-05 17:08:11 +0700

    Создание свойства, отображающего инфу о том, в каком режиме мы работаем: дизайнера или запущенного приложения.

--- App.xaml.cs
+++ App.xaml.cs
@@ -15,11 +15,13 @@ namespace KantorLr1
 	/// </summary>
 	public partial class App : Application
 	{
+		public static bool IsDesignMode { get; private set; } = true;
 		private static IHost host;
 		public static IHost Host => host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs())
 			.Build();
 		protected override async void OnStartup(StartupEventArgs e)
 		{
+			IsDesignMode = false;
 			IHost host1 = Host;
 			base.OnStartup(e);
 			await host1.StartAsync().ConfigureAwait(false);
