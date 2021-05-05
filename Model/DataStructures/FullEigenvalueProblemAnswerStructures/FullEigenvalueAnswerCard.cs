a1343a10d6ab59394124620fee26df33c1720647
Rukin Danil <rukin2018d@yandex.ru>
2021-05-05 14:33:01 +0700

    Полная проблема собственных чисел - viewmodel и view связаны

---
+++ Model/DataStructures/FullEigenvalueProblemAnswerStructures/FullEigenvalueAnswerCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CompMathLibrary.Extensions;
+using KantorLr1.Model.Extensions;
+
+namespace KantorLr1.Model.DataStructures.FullEigenvalueProblemAnswerStructures
+{
+	public class FullEigenvalueAnswerCard
+	{
+		public double Eigenvalue { get; set; }
+		public string Eigenvector { get; set; }
+		public string Residuals { get; set; }
+
+		public FullEigenvalueAnswerCard(double eigenvalue, double[] eigenvector, double[] residuals)
+		{
+			Eigenvalue = eigenvalue;
+			Eigenvector = eigenvector.GetEquivalentString();
+			Residuals = residuals.GetEquivalentString();
+		}
+	}
+}
